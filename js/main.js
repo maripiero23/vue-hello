483445d28188589d303afcf55f6adf582d11e26a
maripiero23 <mari.pieropan@gmail.com>
2022-11-21 15:25:37 +0100

    v-bind

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Dall'oggetto globale Vue, estrai in una variabile la funzione "createApp"
+const { createApp } = Vue;
+
+
+createApp({
+  // data: function () {}
+  data () {
+    // la funzione data deve SEMPRE ritornare un oggetto
+    return {
+        txt: "il mio primo esercizio con Vue con il mio amico Mario",
+        fotoMario: "img/mario.jpeg",
+        flexare: d-flex,
+        centratura:justify-content-center
+    };
+  },
+}).mount("#app")
